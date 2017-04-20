9fe2c85a04acb0d547a14e7503b026a22594c1c4
MasoudA <masoud_nitro@yahoo.com>
2017-04-20 13:11:39 +0800

    initial Release

---
+++ src/Sitecore.Support.109938/DropListField.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Forms.Mvc.ViewModels.Fields;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.Forms.Mvc.ViewModels.Fields
+{
+    public class DropListField : RadioListField
+    {
+        public bool EmptyChoise { get; set; }
+
+        public DropListField()
+        {
+            this.EmptyChoise = true;
+        }
+
+        public override void Initialize()
+        {
+            if (this.Parameters.ContainsKey("emptychoice"))
+                this.EmptyChoise = this.Parameters["emptychoice"].Equals("yes", StringComparison.InvariantCultureIgnoreCase);
+            if (this.Items == null)
+                this.Items = new List<SelectListItem>();
+            if (this.EmptyChoise)
+                this.Items.Insert(0, new SelectListItem()
+                {
+                    Text = string.Empty,
+                    Value = string.Empty
+                });
+            base.Initialize();
+        }
+    }
+}
---
+++ src/Sitecore.Support.109938/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.109938")]
+[assembly: AssemblyProduct("Sitecore.Support.109938")]
+[assembly: ComVisible(false)]
